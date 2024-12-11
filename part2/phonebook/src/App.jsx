1c367421161a6195f83503479a5766a8f19a043f
Adrian DeRose <adrianderose.dev@gmail.com>
2024-12-10 17:27:58 -0800

    add names to phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,19 +6,40 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('form submitted: ', newName);
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input type='text' value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
+        <div>debug: {newName}</div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {
+        persons.map(person => {
+          return <p key={person.name}>{person.name}</p>
+        })
+      }
     </div>
   )
 }
